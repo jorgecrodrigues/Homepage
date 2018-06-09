f685638f34e9b4435d6e22fa123120498e48323d
Jorge Rodrigues <jorgerodrigues9@outlook.com>
2018-06-09 10:21:16 -0400

    Adicionado animação de carregamento. Alterado layout padrão.

--- themes/homepage/assets/js/scripts.js
+++ themes/homepage/assets/js/scripts.js
@@ -3,6 +3,16 @@
         window.console.error("Jquery is required!");
     }
 
+    // ON DOCUMENT READY
+    j(window.document).ready(function () {
+        j(window.document.body).addClass('ready');
+    });
+
+    // ON DOCUMENT LOAD
+    j(window).on('load', function (e) {
+       j(window.document.body).addClass('loaded');
+    });
+
     j('.owl-carousel').owlCarousel({
         margin: 10,
         loop: false,
